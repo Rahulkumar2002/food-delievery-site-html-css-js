f66847f892199960c1ee267898f3e5350b00d2f5
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2021-11-24 10:55:35 -0800

    Initial commit

---
+++ indxxe.js
@@ -0,0 +1,72 @@
+console.log("this is a greater than function");
+// function size(a,b) d
+// {
+//     if (a > b) {
+//        console.log("this is the maximum value " + a);
+//     }
+//     else{
+//        console.log("this is the maximum value " + b)
+//     }
+// };
+// let a = 7;
+// let b = 6;
+// size(a,b);
+
+
+
+// let age = prompt("what us your age","input your age");
+// size(age);
+
+// function size(age) 
+// {
+//     if (age >= 18) {
+//        console.log("you are allowed");
+//     }
+//     else{
+//        console.log("you are not allowed");
+//     }
+// }
+
+// let a =  new Array("Rahul", "Rohan", "Sanjeev","Death");
+// console.log(a);
+// a.forEach(function age(e){
+//     console.log("Hello Friend, "+ e + " this is from javascript")
+// })
+
+// for (let i = 0; i < a.length; i++) {
+//     console.log("hello friend "+ a[i] );
+//     if (i=== 3) {
+//         a.push("Rajiv");
+//     }
+// }
+
+let allowed = document.getElementsByClassName("item");
+console.log(allowed);
+allowed.innerHTML = "<B>THIS</B>";
+console.log(allowed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
